90622a13fef07247959fbf632d22c1e95268e91c
siyanz <siyanz@F15PHD23.HCII.CS.CMU.EDU>
2015-11-19 14:50:12 -0500

    Code cleanup; Detection and orientation functioning

--- device-detection/obj_track.py
+++ device-detection/obj_track.py
@@ -5,105 +5,103 @@ import imutils
 import cv2
 import zhuocv as zc
 import helper
+from matplotlib import pyplot as plt
+import datetime
 
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-v", "--video",
-# help = "path to the (optional) video file")
-# args = vars(ap.parse_args())
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = False, help = "Path to the image")
-args = vars(ap.parse_args())
+IMG_SIZE = 600.0
+BLUR_FACTOR = 3
+MONITOR_RATIO = 1.456
 
 
-blueLower = np.array([110, 60, 30])
-blueUpper = np.array([130, 255, 255])
+def colorThreshMask(frame, lowVal, highVal):
+    #convert to hsv and apply color threshold
+    #returns a mask of the frame based on the low and high threshold values
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    blue = cv2.inRange(hsv, lowVal, highVal)
 
-if not args.get("image", False):
-    camera = cv2.VideoCapture(0)
+    return blue
 
-while True:
-    (grabbed, frame) = camera.read()
+def warpFrame(frame, rect, mask):
+    # Given a frame and a points of a rectangular contour, warp the rectangle to straighten
+    # returns (warped image of the mask, warped image of the frame)
 
-    if not grabbed:
-        break
+    #create a new mask for the warped image
+    mask_small = np.zeros(frame.shape[:2], dtype=np.uint8)
 
-    # frame = cv2.imread(args["image"])
+    cv2.drawContours(mask_small, [rect], -1, 255, -1)
 
-    IMG_SIZE = 600.0
-    BLUR_FACTOR = 3
-    RESIZE_RATIO = frame.shape[0]/IMG_SIZE
-    MONITOR_RATIO = 1.456
+    ## WARP THE MONITOR
+    pts = rect.reshape(4, 2)
+    warp_rect = np.zeros((4, 2), dtype = "float32")
+    s = pts.sum(axis=1)
+    warp_rect[0] = pts[np.argmin(s)]
+    warp_rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis = 1)
+    warp_rect[1] = pts[np.argmin(diff)]
+    warp_rect[3] = pts[np.argmax(diff)]
+
+    # now that we have our rectangle of points, let's compute
+    # the width of our new image
+    (tl, tr, br, bl) = warp_rect
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+
+    # ...and now for the height of our new image
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+
+    # take the maximum of the width and height values to reach
+    # our final dimensions
+    maxWidth = max(int(widthA), int(widthB))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # construct our destination points which will be used to
+    # map the screen to a top-down, "birds eye" view
+    dst = np.array([
+    	[0, 0],
+    	[maxWidth - 1, 0],
+    	[maxWidth - 1, maxHeight - 1],
+    	[0, maxHeight - 1]], dtype = "float32")
+
+    # calculate the perspective transform matrix and warp
+    # the perspective to grab the screen
+    M = cv2.getPerspectiveTransform(warp_rect, dst)
+    warp = cv2.warpPerspective(mask, M, (maxWidth, maxHeight))
+    warp_frame = cv2.warpPerspective(frame, M, (maxWidth, maxHeight))
+
+    return (warp, warp_frame)
+
+def detect_monitor(frame):
     MONITOR_PRESENT = False
     MID_BUTTON_PRESENT = False
+    UPRIGHT = False
 
+    blueLower = np.array([110, 60, 30])
+    blueUpper = np.array([130, 255, 255])
+    # frame = cv2.imread(args["image"])
     frame = imutils.resize(frame, width = int(IMG_SIZE))
+    blue = colorThreshMask(frame, blueLower, blueUpper)
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    blue = cv2.inRange(hsv, blueLower, blueUpper)
-
+    # ZHUOCV's implementation
     # DoB = zc.get_DoB(hsv, 8, 1, method = "Average")
     # mask_blue = zc.color_inrange(DoB, 'HSV', H_L = 0, H_U = 120, S_L = 50, S_U = 255, V_L = 40, V_U = 105)
     # mask_blue2 = zc.color_inrange(DoB, 'HSV', H_L = 0, H_U = 90, S_L = 0, S_U = 200, V_L = 40, V_U = 105)
+
     res = cv2.bitwise_and(frame,frame, mask = blue)
-
     blue = cv2.GaussianBlur(blue, (BLUR_FACTOR, BLUR_FACTOR), 0)
-
     (_, cnts, _) = cv2.findContours(blue.copy(),
     cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    mask_small = np.zeros(frame.shape[:2], dtype=np.uint8)
-
     if len(cnts) > 0:
+        MONITOR_PRESENT = True
         cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
         rect = np.int32(cv2.boxPoints(cv2.minAreaRect(cnt)))
 
-        cv2.drawContours(mask_small, [rect], -1, 255, -1)
-        MONITOR_PRESENT = True
+        (warp, warp_frame) = warpFrame(frame, rect, blue)
 
-        ## WARP THE MONITOR
-        pts = rect.reshape(4, 2)
-        warp_rect = np.zeros((4, 2), dtype = "float32")
-        s = pts.sum(axis=1)
-        warp_rect[0] = pts[np.argmin(s)]
-        warp_rect[2] = pts[np.argmax(s)]
-
-        diff = np.diff(pts, axis = 1)
-        warp_rect[1] = pts[np.argmin(diff)]
-        warp_rect[3] = pts[np.argmax(diff)]
-
-        # now that we have our rectangle of points, let's compute
-        # the width of our new image
-        (tl, tr, br, bl) = warp_rect
-        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-
-        # ...and now for the height of our new image
-        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-
-        # take the maximum of the width and height values to reach
-        # our final dimensions
-        maxWidth = max(int(widthA), int(widthB))
-        maxHeight = max(int(heightA), int(heightB))
-
-        # construct our destination points which will be used to
-        # map the screen to a top-down, "birds eye" view
-        dst = np.array([
-        	[0, 0],
-        	[maxWidth - 1, 0],
-        	[maxWidth - 1, maxHeight - 1],
-        	[0, maxHeight - 1]], dtype = "float32")
-
-        # calculate the perspective transform matrix and warp
-        # the perspective to grab the screen
-        M = cv2.getPerspectiveTransform(warp_rect, dst)
-        warp = cv2.warpPerspective(blue, M, (maxWidth, maxHeight))
-        warp_frame = cv2.warpPerspective(frame, M, (maxWidth, maxHeight))
-
-        ## work with warp from here on ##
-        ## Check monitor orientation
+        # work with warp from here on ##
+        # Check monitor orientation
         if (warp.shape[0] < warp.shape[1]):
             orientation = 'landscape'
         else:
@@ -116,8 +114,6 @@ while True:
         else:
             monitor_mid_pos = (0, rect[2, 1] + monitor_width/2.0)
         # print "width: %d; height: %d" % (monitor_width, monitor_height)
-        # print orientation
-        # print monitor_mid_pos
 
         ## GET CIRCULAR BUTTONS ON THE MONITOR
         # res2 = cv2.bitwise_and(blue, blue, mask = mask_small)
@@ -128,7 +124,6 @@ while True:
         #[(center, radius), (center, radius), ...]
         possible_button = []
         for cnt_idx, cnt in enumerate(comp_cnts):
-            # print(cnt)
             approx = cv2.approxPolyDP(cnt,0.02*cv2.arcLength(cnt,True),True)
 
             if (len(approx) > 5) and (len(approx) < 12):
@@ -136,14 +131,16 @@ while True:
                 center = (int(x),int(y))
                 radius = int(radius)
                 # print ("diameter: %d; 1/6 of monitor width: %d" % (radius * 2, monitor_width/6))
+
                 # Button can't be too small
-                if (radius * 2 >= monitor_width/6) and (radius * 2 < monitor_width):
+                if (radius * 2 >= monitor_width/6) and (radius * 2 < monitor_width/2):
                     possible_button.append((center, radius))
                 # img = cv2.circle(warp_frame,center,radius,(0,255,0),2)
 
         ## check if it's the middle button
         if len(possible_button) == 0:
-            print "Error: no button found!"
+            pass
+            # print "Error: no button found!"
         else:
             for button in possible_button:
                 # print button
@@ -156,31 +153,77 @@ while True:
                     if monitor_width/2 - delta <= center[0] <= monitor_width/2 + delta:
                         # print ("button x: %d; monitor mid: %d; range: (%d, %d)" %
                         #         (center[0], monitor_mid_pos[0], monitor_mid_pos[0] - delta, monitor_mid_pos[0] + delta))
+                        if (center[1] > monitor_height/2):
+                            UPRIGHT = True
                         img = cv2.circle(warp_frame,center,radius,(0,255,0),2)
                         MID_BUTTON_PRESENT = True
                 if orientation == 'landscape':
-                    if ((monitor_width/2 - delta) <= center[1] <= (monitor_width/2 + delta)):
+                    if ((monitor_width/2 - delta) <= center[1]
+                        <= (monitor_width/2 + delta)):
                         # print (monitor_width/2, center[1])
                         img =  cv2.circle(warp_frame,center,radius,(0,255,0),2)
                         MID_BUTTON_PRESENT = True
 
             # cv2.imshow("warp_frame", warp_frame)
     if (MID_BUTTON_PRESENT and MONITOR_PRESENT):
-        print("detect monitor at orientation: %s" % orientation)
-        cv2.drawContours(frame, [rect], -1, 255, 2)
+        if (detect_screen_check(warp_frame)):
+            print("{:%Y-%b-%d %H:%M:%S}: detect monitor at orientation: {}".format(datetime.datetime.now(),orientation))
+            cv2.drawContours(frame, [rect], -1, 255, 2)
+
+        cv2.imshow("warp", warp_frame)
+
+    # SIFT_check(warp_frame)
     # cv2.imshow("res", DoB)
     # cv2.imshow("mask", blue)
-    # cv2.imshow("mask2", res2)
-    # cv2.imshow("warp", warp)
-    cv2.imshow("frame" , frame)
+    cv2.imshow("frame", frame)
+    # cv2.imwrite(args["image"] + "mask.jpg", blue)
+    # cv2.imwrite(args["image"] + "small_mask.jpg", warp)
+    # cv2.imwrite(args["image"] + "detect.jpg", warp_frame)
 
-# cv2.imwrite(args["image"] + "mask.jpg", blue)
-# cv2.imwrite(args["image"] + "small_mask.jpg", warp)
-# cv2.imwrite(args["image"] + "detect.jpg", warp_frame)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
-        break
-# cv2.waitKey(0)
+def detect_screen_check(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    size = frame.shape[0] * frame.shape[1]
 
+    edges = cv2.Canny(gray, 90, 150)
+    kernel = np.ones((2,2),np.uint8)
+    edges = cv2.dilate(edges,kernel,iterations = 1)
+    # opening = cv2.morphologyEx(edges, cv2.MORPH_OPEN, kernel)
 
-camera.release()
-cv2.destroyAllWindows()
+    # cv2.imshow("edges", edges)
+
+    (_, cnts, _) = cv2.findContours(edges.copy(),
+    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    if len(cnts) > 0:
+        cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+        cv2.drawContours(frame, [cnt], -1, 255, 2)
+        peri = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.03 * peri, True)
+
+        if (len(approx) == 4 and (size/3*2 < cv2.contourArea(cnt))):
+            return True
+        # print(len(approx))
+    return False
+    
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required = False, help = "Path to the image")
+    args = vars(ap.parse_args())
+    frame = cv2.imread(args["image"])
+
+    if not args.get("image", False):
+        camera = cv2.VideoCapture(0)
+
+    while True:
+        (grabbed, frame) = camera.read()
+
+        if not grabbed:
+            break
+        detect_monitor(frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    # cv2.waitKey(0)
+
+    camera.release()
+    cv2.destroyAllWindows()
